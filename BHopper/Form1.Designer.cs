f7b70ac85c55d81cae436c3593745f75dbaadeb0
borfus <mrpeteyb@gmail.com>
2018-04-06 18:47:03 -0500

    Add project files.

---
+++ BHopper/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BHopper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnabled = new System.Windows.Forms.Button();
+            this.lblJumping = new System.Windows.Forms.Label();
+            this.prgJumping = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnEnabled
+            // 
+            this.btnEnabled.Location = new System.Drawing.Point(12, 12);
+            this.btnEnabled.Name = "btnEnabled";
+            this.btnEnabled.Size = new System.Drawing.Size(94, 23);
+            this.btnEnabled.TabIndex = 0;
+            this.btnEnabled.TabStop = false;
+            this.btnEnabled.Text = "Enable";
+            this.btnEnabled.UseVisualStyleBackColor = true;
+            this.btnEnabled.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEnabled_MouseClick);
+            // 
+            // lblJumping
+            // 
+            this.lblJumping.AutoSize = true;
+            this.lblJumping.Location = new System.Drawing.Point(112, 17);
+            this.lblJumping.Name = "lblJumping";
+            this.lblJumping.Size = new System.Drawing.Size(52, 13);
+            this.lblJumping.TabIndex = 1;
+            this.lblJumping.Text = "Jumping: ";
+            // 
+            // prgJumping
+            // 
+            this.prgJumping.BackColor = System.Drawing.Color.White;
+            this.prgJumping.ForeColor = System.Drawing.Color.Red;
+            this.prgJumping.Location = new System.Drawing.Point(168, 12);
+            this.prgJumping.Name = "prgJumping";
+            this.prgJumping.Size = new System.Drawing.Size(40, 23);
+            this.prgJumping.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 44);
+            this.Controls.Add(this.prgJumping);
+            this.Controls.Add(this.lblJumping);
+            this.Controls.Add(this.btnEnabled);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BHopper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnabled;
+        private System.Windows.Forms.Label lblJumping;
+        private System.Windows.Forms.ProgressBar prgJumping;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ BHopper/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace BHopper
+{
+    public partial class Form1 : Form
+    {
+        int addressLocalPlayer;
+        int offsetFlags;
+        int addressJump;
+
+        string process = "csgo";
+        int client;
+
+        bool enabled = false;
+
+        [DllImport("user32", CharSet = CharSet.Ansi, SetLastError = true)]
+        public static extern int GetAsyncKeyState(int vKey);
+
+        public Form1()
+        {
+            InitializeComponent();
+            prgJumping.Style = ProgressBarStyle.Continuous;
+        }
+
+        private void btnEnabled_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!enabled)
+            {
+                enabled = true;
+                btnEnabled.Text = "Disable";
+                prgJumping.Value = 100;
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                if (!backgroundWorker1.IsBusy)
+                {
+                    backgroundWorker1.CancelAsync();
+                }
+                enabled = false;
+                btnEnabled.Text = "Enable";
+                prgJumping.Value = 0;
+            }
+            this.ActiveControl = null;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true && enabled)
+            {
+                while (GetAsyncKeyState(32) < 0)
+                {
+                    if (prgJumping.ForeColor != Color.Green)
+                    {
+                        MethodInvoker mi = delegate ()
+                        {
+                            prgJumping.ForeColor = Color.Green;
+                        };
+                        this.Invoke(mi);
+                    }
+
+                    Thread.Sleep(10);
+                }
+                MethodInvoker mi2 = delegate ()
+                {
+                    prgJumping.ForeColor = Color.Red;
+                };
+                this.Invoke(mi2);
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
